58b6638cf55caaa72c692ddd975b54556b69d1b5
WYeast <56200018+WYeast@users.noreply.github.com>
2019-10-15 03:59:19 +0800

    Update write_fasta.pl

--- write_fasta.pl
+++ write_fasta.pl
@@ -9,22 +9,22 @@ my %hash;
 my $id;
 my $seq;
 while(<IN>){
-	if($_=~/>/){
-		$id=substr($_,1,15);
-	    $hash{$id}=$seq;
-	    next;
-	}
-	chomp;
-	$seq.="$_\n";
+    if($_=~/>/){
+    $id=substr($_,1,15);
+    $hash{$id}=$seq;
+    next;
+    }
+    chomp;
+    $seq.="$_\n";
 }
 	    
 my $i=0;
 while(<IN2>){
-	chomp;
-	my @sp=split /\s+/;
-	if($hash{$sp[0]}){
-		print OUT "$sp[0]\n$hash{$sp[0]}";
-		$i++;
-	}
+    chomp;
+    my @sp=split /\s+/;
+    if($hash{$sp[0]}){
+    print OUT "$sp[0]\n$hash{$sp[0]}";
+    $i++;
+    }
 }
 print "$i";
